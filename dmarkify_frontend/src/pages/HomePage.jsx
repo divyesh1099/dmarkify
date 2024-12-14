faf9b8548b968f874088cb294ec97a70e1bff531
Divyesh Vishwakarma <divyesh1099@gmail.com>
2024-12-15 05:24:05 +0530

    Update Correct Link

--- dmarkify_frontend/src/pages/HomePage.jsx
+++ dmarkify_frontend/src/pages/HomePage.jsx
@@ -558,7 +558,7 @@ const HomePage = () => {
       )}
 
       <footer className="bg-gray-100 text-center py-4 mt-4">
-        <p className="text-sm text-gray-500">© 2024 Dmarkify by <a href="www.linkedin.com/in/motidivya">Divyesh Vishwakarma</a>. All rights reserved. (❤️Moti)</p>
+        <p className="text-sm text-gray-500">© 2024 Dmarkify by <a href="https://www.linkedin.com/in/motidivya">Divyesh Vishwakarma</a>. All rights reserved. (❤️Moti)</p>
       </footer>
     </div>
   );
